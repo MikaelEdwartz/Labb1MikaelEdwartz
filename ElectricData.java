fbd69950b6e10b12952f7838676626dd5c4ab869
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-09-06 20:17:55 +0200

    add clean code

--- ElectricData.java
+++ ElectricData.java
@@ -7,21 +7,13 @@ public class ElectricData {
     public ElectricData(int time, int price){
         this.time = time;
         this.electricPrice = price;
-
     }
 
     public int getTime(){
-
         return this.time;
-
     }
-
 
     public int getPrice(){
         return this.electricPrice;
-    }
-
-    public String toString(){
-        return "Mellan kl " + this.time + " är priset: " + this.electricPrice;
     }
 }
