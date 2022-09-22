d69b5f9d9938564d50f13dc61101778056f8cefc
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-09-22 18:07:11 +0200

    add convert to record

--- labb2/ProductCategory.java
+++ labb2/ProductCategory.java
@@ -1,26 +1,12 @@
 package se.iths.labborationer.labb2;
 
-public class ProductCategory {
-
-    private final String category;
-
-    public ProductCategory(String category){
-        this.category = category;
-
-
-    }
-
-
-    public String getCategory() {
-        return category;
-    }
+public record ProductCategory(String category) {
 
 
     @Override
     public String toString() {
         return category;
     }
-
 
 
 }
