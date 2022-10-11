65e4160fd88e52fb7fd5af84d22453172f4058d8
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-10-11 09:29:44 +0200

    add code

--- labb2/CustomerInteface.java
+++ labb2/CustomerInteface.java
@@ -23,7 +23,7 @@ public class CustomerInteface{
     }
 
     public void start(boolean loop) {
-        //
+        startupkategorier();
         while (loop) {
 
             costumerMenuGreeting();
@@ -37,104 +37,18 @@ public class CustomerInteface{
                 case "4" -> register.printRegister();
                 case "5" -> checkOut(register);
                 case "e" -> loop = false;
-                case "9" -> this.balance.printbalancetest();
-                case "8" -> removeRegisterItemsFromBalance();
-                case "7" -> startupkategorier();
+
             }
             System.out.println("====================");
         }
     }
-
-    private void checkOut(Register register) {
-//        this.register.add(new Product(new ProductCategory("Dairy"), "Milk", valueOf(199), 10938));
-//        this.register.add(new Product(new ProductCategory("Dairy"), "Milk", valueOf(199), 10938));
-//        this.register.add(new Product(new ProductCategory("Dairy"), "Cream", valueOf(11), 1098));
-//        this.register.add(new Product(new ProductCategory("Meat"), "Chicken", valueOf(12), 109));
-//        this.register.add(new Product(new ProductCategory("Meat"), "Chicken", valueOf(12), 109));
-
-
-        var distinctRegister = new Register(register.getDistinctRegister(register.register));
-        var priceToPay = valueOf(0);
-            System.out.println("Produkt"+printTab(3)+ "á-pris" +printTab(2)+"st\t\ttotalpris");
-
-        for (int i = 0; i < distinctRegister.size(); i++) {
-
-            System.out.println(distinctRegister.getProduct(i).product()
-                 + printTab(3) + distinctRegister.getProduct(i).price()
-                 + printTab(3) + nrOfProducts(distinctRegister.getProduct(i))
-                 + printTab(2) + totalPrice(register, distinctRegister.getProduct(i)));
-
-         priceToPay = priceToPay.add(totalPrice(register, distinctRegister.getProduct(i)));
-
-        }
-        System.out.println();
-
-        BigDecimal discountedPrice = priceToPay;
-
-        if(priceToPay.compareTo(valueOf(2000)) > 0)
-            discountedPrice = applyDiscount(new TwentyPercent(), priceToPay);
-        else if(priceToPay.compareTo(valueOf(1000)) > 0)
-            discountedPrice = applyDiscount(new TenPercent(), priceToPay);
-
-        System.out.println("Totalt " + printTab(8)+ priceToPay + " kr");
-        printDiscountValue(priceToPay);
-        System.out.println(printTab(9) + "__________");
-        System.out.println("Att betala "+ printTab(7) + discountedPrice + " kr");
-        System.out.println(priceToPay);
-
-
-
-        /*
-        Produkt      á-pris     st      totalpris
-        morot        1.79       4            7.16
-        kyckling     2.99       2            5.98
-        Bröd         1          2            2.40
-
-        Totalt                              72.39
-        Discount                            -9.60
-                                       ----------
-        Att betala                          65.69
-
-        Payment                              70.00
-        Your Change                    4.31
-
-        Thank you. Come Again!*/
-
+    private void costumerMenuGreeting() {
+        System.out.println("1. Vill gå igenom alla kategorier en i taget?");
+        System.out.println("2. Välj en specifik kategori");
+        System.out.println("3. Printa ut alla produkter");
+        System.out.println("4. print your register");
+        System.out.println("e. Gå till kassan och betala");
     }
-
-    private void removeRegisterItemsFromBalance(){
-        var templist = new ArrayList<>(balance.getInventory());
-        for (int i = 0; i < register.size(); i++) {
-          this.balance.remove(this.register.getProduct(i));
-        }
-    }
-    private  void printDiscountValue(BigDecimal priceToPay) {
-       var discount = valueOf(0);
-        if(priceToPay.compareTo(valueOf(2000)) > 0)
-             discount = priceToPay.multiply(valueOf(0.2));
-        else if(priceToPay.compareTo(valueOf(1000)) > 0)
-             discount = priceToPay.multiply(valueOf(0.1));
-
-        if(discount.compareTo(valueOf(1)) > 0)
-            System.out.println("Rabatt " + printTab(8) + discount + " kr");
-    }
-
-    private BigDecimal applyDiscount(Discounter discount, BigDecimal amount){
-        return discount.apply(amount);
-
-    }
-
-    private BigDecimal totalPrice(Register register, Product product) {
-        var nrOfProducts = valueOf(nrOfProducts(product));
-        return product.price().multiply(nrOfProducts);
-
-    }
-
-    private int nrOfProducts(Product product){
-        return (int) register.sameProductsInRegister(product);
-    }
-
-
 
     private void loopThroughCategories(Register register) {
         for (int i = 0; i < this.categories.size(); i++) {
@@ -151,18 +65,18 @@ public class CustomerInteface{
             askAndAddProduct(register, list, choice);
         }
     }
-
-
-    private void addToRegister(Register register, InventoryBalance list) {
-        printProductsInStore(register, list);
-        int choice = getProductOrQuit();
-        askAndAddProduct(register, list, choice);
+    private void printProductsInStore(Register register, InventoryBalance list) {
+        this.count = 1;
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + " " + list.getProduct(i) + " " + actualProductsInStore(list, register, i));
+            count++;
+        }
+        System.out.println(count + " För att gå tillbaka");
     }
-
     private int getProductOrQuit() {
         int choice = getChoice("Skriv siffran på produkten du vill ha eller gå tillbaka.");
         if (choice == count)
-             start(true);
+            start(true);
         return choice;
     }
 
@@ -174,91 +88,6 @@ public class CustomerInteface{
         else
             System.out.println("Finns inte tillräckligt med varor");
     }
-
-    private ProductCategory getCategoryAtIndex(int i) {
-        return this.categories.get(i);
-    }
-
-    private void getSpecificCategor(Register register) {
-        var categoryChoice = getUserCategoryChoice(1);
-
-        var list = new InventoryBalance(this.balance.getListWithChosenCategory(categoryChoice));
-
-            addToRegister(register, list);
-
-    }
-    private ProductCategory getUserCategoryChoice(int number) {
-        printCategoriesInOrder(number);
-        return this.categories.get(scanner.nextInt() - 1);
-    }
-    private void printCategoriesInOrder(int number) {
-        if (this.categories.size() >= 1) {
-            printCategoriesFormated(number);
-        }
-    }
-    private void printCategoriesFormated(int number) {
-        for (int i = 0; i < this.categories.size(); i++) {
-            System.out.println((i + number) + ". " + getCategoryAtIndex(i) + ".");
-        }
-    }
-
-    private void searchByCategory() {
-
-    }
-    private void costumerMenuGreeting() {
-        System.out.println("1. Vill gå igenom alla kategorier en i taget?");
-        System.out.println("2. Välj en specifik kategori");
-        System.out.println("3. Printa ut alla produkter");
-        System.out.println("4. print your register");
-        System.out.println("e. Gå till kassan och betala");
-    }
-
-    private void printCategory(ProductCategory category) {
-        this.balance.printProductWithCategory(category);
-    }
-
-
-
-    private void listAllProducts(Register register) {
-
-        var list = new InventoryBalance(this.balance.getDistinctProducts(this.balance.getInventory()));
-           while(true)
-                addToRegister(register, list);
-
-    }
-
-
-    private void printProductsInStore(Register register, InventoryBalance list) {
-        this.count = 1;
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println((i + 1) + " " + list.getProduct(i) + " " + actualProductsInStore(list, register, i));
-            count++;
-        }
-        System.out.println(count + " För att gå tillbaka");
-    }
-
-    private boolean isProductAvailable(Register register, InventoryBalance list, int choice, long nrOfProducts) {
-        return nrOfProducts <= actualProductsInStore(list, register, (choice - 1)) && actualProductsInStore(list, register, (choice - 1)) >= 0;
-    }
-
-    private long getNrOfProducts(String x) {
-        System.out.println(x);
-        long nrOfProducts = scanner.nextInt();
-        return nrOfProducts;
-    }
-
-    private int getChoice(String x) {
-        System.out.println(x);
-        int choice = scanner.nextInt();
-        return choice;
-    }
-
-    private static void addNrOfProductsToRegister(Register register, InventoryBalance list, int choice, long nrOfProducts) {
-        for (int i = 0; i < nrOfProducts; i++) {
-            register.add(list.getProduct(choice - 1));
-        }
-    }
-
     private int actualProductsInStore(InventoryBalance list, Register register, int i ){
         int produtsInStore = (int) this.balance.nrOfProducts(list.getProduct(i));
 
@@ -267,6 +96,191 @@ public class CustomerInteface{
         }
         return produtsInStore;
     }
+
+    private int getChoice(String x) {
+        System.out.println(x);
+        int choice = scanner.nextInt();
+        return choice;
+    }
+    private long getNrOfProducts(String x) {
+        System.out.println(x);
+        long nrOfProducts = scanner.nextInt();
+        return nrOfProducts;
+    }
+    private boolean isProductAvailable(Register register, InventoryBalance list, int choice, long nrOfProducts) {
+        return nrOfProducts <= actualProductsInStore(list, register, (choice - 1)) && actualProductsInStore(list, register, (choice - 1)) >= 0;
+    }
+    private static void addNrOfProductsToRegister(Register register, InventoryBalance list, int choice, long nrOfProducts) {
+        for (int i = 0; i < nrOfProducts; i++) {
+            register.add(list.getProduct(choice - 1));
+        }
+    }
+    //__________________________________________________________________________________
+    private void getSpecificCategor(Register register) {
+        var categoryChoice = getUserCategoryChoice(1);
+
+        var list = new InventoryBalance(this.balance.getListWithChosenCategory(categoryChoice));
+
+        addToRegister(register, list);
+
+    }
+    private ProductCategory getUserCategoryChoice(int number) {
+        printCategoriesInOrder(number);
+        return this.categories.get(scanner.nextInt() - 1);
+    }
+
+    private void addToRegister(Register register, InventoryBalance list) {
+        printProductsInStore(register, list);
+        int choice = getProductOrQuit();
+        askAndAddProduct(register, list, choice);
+    }
+    private void printCategoriesInOrder(int number) {
+        if (this.categories.size() >= 1) {
+            printCategoriesFormated(number);
+        }
+    } private void printCategoriesFormated(int number) {
+        for (int i = 0; i < this.categories.size(); i++) {
+            System.out.println((i + number) + ". " + getCategoryAtIndex(i) + ".");
+        }
+    }
+    private ProductCategory getCategoryAtIndex(int i) {
+        return this.categories.get(i);
+    }
+    //__________________________________________________________________________________
+    private void listAllProducts(Register register) {
+
+        var list = new InventoryBalance(this.balance.getDistinctProducts(this.balance.getInventory()));
+        while(true)
+            addToRegister(register, list);
+
+    }
+
+    //__________________________________________________________________________________
+
+    private void checkOut(Register register) {
+
+
+        var distinctRegister = new Register(register.getDistinctRegister(register.register));
+        var priceToPay = valueOf(0);
+            System.out.println("Produkt"+ printTab(3) + "á-pris" + printTab(2) + "st"+printTab(2)+"totalpris");
+
+        for (int i = 0; i < distinctRegister.size(); i++) {
+
+            System.out.println(distinctRegister.getProduct(i).product()
+                 + printTab(3) + distinctRegister.getProduct(i).price()
+                 + printTab(3) + nrOfProducts(distinctRegister.getProduct(i))
+                 + printTab(2) + totalPrice(distinctRegister.getProduct(i)));
+
+         priceToPay = priceToPay.add(totalPrice(distinctRegister.getProduct(i)));
+
+        }
+        System.out.println();
+
+        BigDecimal discountedPrice = priceToPay;
+
+        discountedPrice = getDiscountedPrice(priceToPay, discountedPrice);
+
+        System.out.println("Ord. pris " + printTab(7)+ priceToPay + " kr");
+        printDiscountValue(priceToPay);
+        System.out.println(printTab(9) + "__________");
+        System.out.println("Att betala "+ printTab(7) + discountedPrice + " kr");
+
+
+        removeRegisterItemsFromBalance();
+
+    }
+
+    private BigDecimal getDiscountedPrice(BigDecimal priceToPay, BigDecimal discountedPrice) {
+        if(priceToPay.compareTo(valueOf(2000)) > 0)
+            discountedPrice = applyDiscount(new TwentyPercent(), priceToPay);
+        else if(priceToPay.compareTo(valueOf(1000)) > 0)
+            discountedPrice = applyDiscount(new TenPercent(), priceToPay);
+        return discountedPrice;
+    }
+
+    private String printTab(int nrOfTabs){
+        String tabs = "";
+        for (int j = 0; j < nrOfTabs; j++) {
+            tabs = tabs + "\t";
+        }
+
+        return tabs;
+
+    }
+    private BigDecimal totalPrice( Product product) {
+        var nrOfProducts = valueOf(nrOfProducts(product));
+        return product.price().multiply(nrOfProducts);
+
+    }
+    private  void printDiscountValue(BigDecimal priceToPay) {
+        var discount = getDiscount(priceToPay);
+
+        if(discount.compareTo(valueOf(1)) > 0)
+            System.out.println("Rabatt " + printTab(8) + discount + " kr");
+    }
+
+    private static BigDecimal getDiscount(BigDecimal priceToPay) {
+        var discount = valueOf(0);
+
+        if(priceToPay.compareTo(valueOf(2000)) > 0)
+             discount = priceToPay.multiply(valueOf(0.2));
+        else if(priceToPay.compareTo(valueOf(1000)) > 0)
+             discount = priceToPay.multiply(valueOf(0.1));
+
+        return discount;
+    }
+
+    private BigDecimal applyDiscount(Discounter discount, BigDecimal amount){
+        return discount.apply(amount);
+
+    }
+    private void removeRegisterItemsFromBalance(){
+        for (int i = 0; i < register.size(); i++)
+            this.balance.remove(this.register.getProduct(i));
+
+    }
+
+
+    private int nrOfProducts(Product product){
+        return (int) register.sameProductsInRegister(product);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void searchByCategory() {
+
+    }
+
+    private void printCategory(ProductCategory category) {
+        this.balance.printProductWithCategory(category);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     private void printProducts() {
@@ -299,14 +313,6 @@ public class CustomerInteface{
         }
     }
 
-    private String printTab(int nrOfTabs){
-        String tabs = "";
-        for (int j = 0; j < nrOfTabs; j++) {
-            tabs = tabs + "\t";
-        }
 
-        return tabs;
-
-    }
 }
 
